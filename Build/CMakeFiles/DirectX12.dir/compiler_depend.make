dbd1968551d4e3ab6e98c6a1bfbf89ce9e155c30
IVIosab <mosab.f.r@gmail.com>
2023-10-13 21:22:48 +0300

    Lab 1.4

---
+++ Build/CMakeFiles/DirectX12.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for DirectX12.
+# This may be replaced when dependencies are built.
---
+++ Build/CMakeFiles/Rasterization.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/iviosab/Desktop/sem7/computer-graphics-in-game-development/src/main.cpp" "CMakeFiles/Rasterization.dir/src/main.cpp.o" "gcc" "CMakeFiles/Rasterization.dir/src/main.cpp.o.d"
+  "/home/iviosab/Desktop/sem7/computer-graphics-in-game-development/src/renderer/rasterizer/rasterizer_renderer.cpp" "CMakeFiles/Rasterization.dir/src/renderer/rasterizer/rasterizer_renderer.cpp.o" "gcc" "CMakeFiles/Rasterization.dir/src/renderer/rasterizer/rasterizer_renderer.cpp.o.d"
+  "/home/iviosab/Desktop/sem7/computer-graphics-in-game-development/src/renderer/renderer.cpp" "CMakeFiles/Rasterization.dir/src/renderer/renderer.cpp.o" "gcc" "CMakeFiles/Rasterization.dir/src/renderer/renderer.cpp.o.d"
+  "/home/iviosab/Desktop/sem7/computer-graphics-in-game-development/src/settings.cpp" "CMakeFiles/Rasterization.dir/src/settings.cpp.o" "gcc" "CMakeFiles/Rasterization.dir/src/settings.cpp.o.d"
+  "/home/iviosab/Desktop/sem7/computer-graphics-in-game-development/src/utils/resource_utils.cpp" "CMakeFiles/Rasterization.dir/src/utils/resource_utils.cpp.o" "gcc" "CMakeFiles/Rasterization.dir/src/utils/resource_utils.cpp.o.d"
+  "/home/iviosab/Desktop/sem7/computer-graphics-in-game-development/src/world/camera.cpp" "CMakeFiles/Rasterization.dir/src/world/camera.cpp.o" "gcc" "CMakeFiles/Rasterization.dir/src/world/camera.cpp.o.d"
+  "/home/iviosab/Desktop/sem7/computer-graphics-in-game-development/src/world/model.cpp" "CMakeFiles/Rasterization.dir/src/world/model.cpp.o" "gcc" "CMakeFiles/Rasterization.dir/src/world/model.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
